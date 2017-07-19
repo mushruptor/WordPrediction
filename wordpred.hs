22c76d33af19ee9f8c2709e4b315397e689f5de2
mushruptor <marcel.legler@tu-dresden.de>
2017-07-19 15:38:34 +0200

    worprediction working - little bug in calculating probabilities (addition under non log-Probs -> what is it under log?)

--- wordpred.hs
+++ wordpred.hs
@@ -4,44 +4,45 @@ module Main where
 
 import System.Environment (getArgs)
 import Data.List 
-import Control.Monad.State.Lazy hiding (mapM)
 import Data.Char
 import Data.Function
 import Data.Typeable
+import Text.Read (readMaybe)
 
-data Ngram = Ngram { weight :: Float, ngram :: [String], fallback :: Float}     deriving (Show, Read)
-data PosString = PosString Int Int String                                       deriving (Show, Read)
+data Ngram = Ngram { weight :: Double, ngram :: [String], fallback :: Maybe Double}  deriving (Show)
 
-testList = [Ngram (-1.0) ["abs", "cbd"] (-0.5),
-            Ngram (-1.7) ["titr", "ijr"] (-0.2),
-            Ngram (-1.9) ["titr", "isd"] (-0.2),
-            Ngram (-2.0) ["titr", "idfr", "jaw"] (-0.2),
-            Ngram (-2.1) ["titr", "idfr", "akksk"] (-0.2)]
+testList = [Ngram (-1.0) ["abs", "cbd"] (Just (-0.5)),
+            Ngram (-1.7) ["titr", "ijr"] (Just (-0.2)),
+            Ngram (-1.9) ["idfr", "isd"] (Just (-0.2)),
+            Ngram (-2.0) ["titr", "idfr", "jaw"] (Just (-0.2)),
+            Ngram (-1.0) ["titr", "idfr", "bla"] (Just (-0.2)),
+            Ngram (-2.2) ["titr", "idfr", "blub"] (Just (-0.2)),
+            Ngram (-2.1) ["titr", "idfr", "akksk"] (Just (-0.2)),
+            Ngram (-2.1) ["titr", "idfr", "akksk"] (Just (-0.2))]
 
+-- | Usage: ./wordpred <numberwords> <ngrammodel> <textfile> <line> <column>
 main :: IO ()
 main = do
     args <- getArgs
-    tralala <- parseARPAFile (args !! 1)
-    foo <- parseFile' ((read (args !! 3)) - 1) ((read (args !! 4)) - 1) (args !! 2)  
-    printNgrams (topK (read (args !! 0)) (compareNgram tralala foo))
-    mapM putStrLn foo
-    putStrLn ""
+    contentarpa <- readFile (args !! 1) 
+    let tralala = parseARPAFile contentarpa 
+    content <- readFile (args !! 2)
+    let count = read (args !! 0) :: Int
+    let query = parseFile 5 ((read (args !! 3) :: Int) - 1) ((read (args !! 4) :: Int) - 1) content 
+    let result = take count (compareNgram count tralala query)
+    mapM_ putStrLn $ (map output result)
+    putStrLn "--END--"
 
--- | print ngram
-printNgram :: Ngram -> IO ()
-printNgram (Ngram a ns b) = putStrLn $ show a ++ "  " ++ unwords ns ++ "  "  ++ show b
+-- | append one list to another without (++)
+append :: [a] -> [a] -> [a]
+append xs ys = foldr (\x y -> x:y) ys xs
 
--- | print list of ngrams
-printNgrams :: [Ngram] -> IO [()] 
-printNgrams = mapM printNgram
+output :: (String, Double) -> String
+output (a,b) = a ++ "   " ++ (show b)
 
--- | print the first k ngrams
-printkNgrams :: Int -> [Ngram] -> IO [()]
-printkNgrams k ns = mapM printNgram (take k ns)
-    
 -- | return only consecutive token as list
 parseString :: String -> [String]
-parseString = filter (not . null) . (\x -> map trim x) . (groupBy ((==) `on` isAlpha))
+parseString = filter (not . null) . map trim . (groupBy ((==) `on` isAlpha))
     where
         trim :: String -> String
         trim = f . f
@@ -49,75 +50,96 @@ parseString = filter (not . null) . (\x -> map trim x) . (groupBy ((==) `on` isA
 
 -- | generate a ngram from a string
 parseNgram :: String -> Ngram
-parseNgram s = go 5 (words s)
+parseNgram s = go (words s)
     where
         -- | if n = k there is no fallback weight -> set it to 0
-        go :: Int -> [String] -> Ngram
-        go k (x:xs) 
-          | length xs >= k = Ngram (read x) xs 0.0 
-          | otherwise = Ngram (read x) (init xs) (read (last xs))
+        go :: [String] -> Ngram
+        go xs 
+          | length xs < 3 = Ngram 0.0 [] Nothing
+          | isDouble lastword = Ngram (read $ head xs) (init $ tail xs) lastword
+          | otherwise = Ngram (read $ head xs) (tail xs) lastword
+            where
+                lastword = readMaybe (last xs) :: Maybe Double
+                
+                isDouble :: Maybe Double -> Bool
+                isDouble Nothing = False
+                isDouble (Just a) = True       
 
 -- | parse the .arpa file to a list of ngrams
-parseARPAFile :: String -> IO [Ngram]
-parseARPAFile file = do
-    content <- readFile file
-    let linesOfFile = lines content
-    return $ map parseNgram $ parseLine linesOfFile
+parseARPAFile :: String -> [Ngram]
+parseARPAFile file = map (parseNgram) $ parseLine (lines file)
     where
         parseLine :: [String] -> [String]
-        parseLine = filter (\x -> (head(x) /= '\\') && (take 5 x /= "ngram")) . filter (not . null)
+        parseLine = filter (\x -> (head x /= '\\') && (take 5 x /= "ngram")) . filter (not . null)
 
--- | parse the .arpa file to a list of strings
-parseARPAFile' :: String -> IO [String]
-parseARPAFile' file = do
-    content <- readFile file
-    let linesOfFile = lines content
-    return $ parseLine linesOfFile
+-- | returns the k predecessing words (◔_◔)
+parseFile :: Int -> Int -> Int -> String -> [String]
+parseFile k lin col file = parseLine (lines file)
     where
         parseLine :: [String] -> [String]
-        parseLine = filter (\x -> (head(x) /= '\\') && (take 5 x /= "ngram")) . filter (not . null)
+        parseLine ls = take' k (go lin (parseLine' col (ls !! lin)) ls)
 
--- | parse the text file to a string list with the position of the word occurrence
-parseFile :: String -> IO [PosString]
-parseFile file = do
-    content <- readFile file
-    let linesOfFile = lines content
-    return $ parseLine 1 linesOfFile
-    where
-        parseLine :: Int -> [String] -> [PosString]
-        parseLine _ [] = []
-        parseLine acc (x:xs) = (parseLine' 1 acc (words x)) ++ (parseLine (acc + 1) xs)
-        
-        parseLine' :: Int -> Int -> [String] -> [PosString]
-        parseLine' _ _ [] = []
-        parseLine' acc l (x:xs) = PosString l acc x : (parseLine' (acc + 1) l  xs)
+        parseLine' :: Int -> String -> [String]
+        parseLine' c l 
+          | c == length l = words l
+          | isSpace (l !! c) = words (take c l)
+          | otherwise = init $ words (take c l)
 
--- | only return predecessing words (◔_◔)
-parseFile' :: Int -> Int -> String -> IO [String]
-parseFile' lin col file = do
-    content <- readFile file
-    let linesOfFile = lines content
-    return $ parseLine linesOfFile
-    where
-        parseLine :: [String] -> [String]
-        parseLine xs = parseLine' (xs !! lin)
+        go :: Int -> [String] -> [String] -> [String]
+        go l ws ls
+          | l < 0 = []
+          | l == 0 = ws
+          | length ws + length prevline < k = append (go (l-1) prevline ls) ws
+          | length ws >= k = ws
+          | otherwise = append prevline ws
+            where
+                prevline = parseLine' (length (ls !! (l-1))) (ls !! (l-1))
 
-        parseLine' :: String -> [String]
-        parseLine' xs 
-          | isSpace (xs !! col) =  words (take col xs)
-          | otherwise = init $ words (take col xs)
+        take' :: Int -> [a] -> [a]
+        take' e = reverse . take e . reverse
 
 -- | return top k results sorted descending by ngram weigths
 topK :: Int -> [Ngram] -> [Ngram]
 topK k = take k . reverse . sortOn weight
+
+-- | calls compareNgram' until the number of predictions is reached
+compareNgram :: Int -> [Ngram] -> [String] -> [(String, Double)]
+compareNgram k ns cs
+  | cs == [] = []
+  | length ngrams < k = append ngrams (compareNgram (k - length ngrams) ns (tail cs))
+  | otherwise = ngrams
+    where
+        ngrams = top . group $ compareNgram' ns cs
+        
+        group :: [Ngram] -> [(String, Double)]
+        group = group'' . map group'
+            
+        group' :: Ngram -> (String, Double)
+        group' (Ngram a xs _) = (last xs, a)
+
+        group'' :: [(String, Double)] -> [(String, Double)]
+        group'' [] = []
+        group'' ((a,b):xs) = case index of
+                               Nothing -> (a,b) : (group'' xs)
+                               Just p -> group'' $ replaceNth p (a, b - snd (xs !! p)) xs
+            where
+                index = elemIndex a (map fst xs)
+
+                replaceNth :: Int -> (String, Double) -> [(String, Double)] -> [(String, Double)]
+                replaceNth n newVal (x:xs)
+                  | n == 0 = newVal:xs
+                  | otherwise = x:replaceNth (n-1) newVal xs
+
+        top :: Ord b => [(a,b)] -> [(a,b)]
+        top xs = reverse $ sortBy (compare `on` snd) xs
 
 -- | takes a list of ngrams and a list of strings of consecutive occuring words
 -- and returns all ngrams matching those words
 -- Note that the last word of each ngram is cut in order to predict the next word
 -- Call this function with the wordlist of the already right length for the ngram
 -- it will filter all of non matching length
-compareNgram :: [Ngram] -> [String] -> [Ngram]
-compareNgram ns cs = matching (filter (\n -> length (ngram n) - 1 == length cs) ns) cs 
+compareNgram' :: [Ngram] -> [String] -> [Ngram]
+compareNgram' ns cs = matching (filter (\n -> length (ngram n) - 1 == length cs) ns) cs 
     where
         matching :: [Ngram] -> [String] -> [Ngram]
         matching [] _ = []
@@ -127,7 +149,8 @@ compareNgram ns cs = matching (filter (\n -> length (ngram n) - 1 == length cs) 
           | otherwise = matching ns cs        
         
         reduce :: Ngram -> Ngram
-        reduce (Ngram a xs b) = Ngram a (take (length xs - 1) xs) b
+        reduce (Ngram a xs Nothing) = Ngram a (take (length xs - 1) xs) Nothing
+        reduce (Ngram a xs (Just b)) = Ngram (a + b) (take (length xs - 1) xs) Nothing
 
         areEqualNS :: Ngram -> [String] -> Bool
         areEqualNS (Ngram _ s1 _) s2 = s1 == s2
